d1676d9f0cdff29dee9675dcd02aacba0b4af81c
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2022-12-29 16:01:18 +0100

    feat: carbon components, header, router, favicons

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,27 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Router from 'svelte-spa-router';
+  import Header from './components/Header.svelte';
+
+  import Home from './routes/Home.svelte';
+  import Routine from './routes/Routine.svelte';
+  import Progress from './routes/Progress.svelte';
+  import Settings from './routes/Settings.svelte';
+  import NotFound from './routes/NotFound.svelte';
+
+  const routes = {
+    '/': Home,
+    '/routine': Routine,
+    '/progress': Progress,
+    '/settings': Settings,
+    '*': NotFound
+  };
 </script>
 
+<Header />
+
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <Router {routes} />
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import {
+    Header,
+    HeaderGlobalAction,
+    HeaderUtilities,
+    SideNav,
+    SideNavItems,
+    SideNavLink,
+    SkipToContent
+  } from 'carbon-components-svelte';
+  import Play from 'carbon-icons-svelte/lib/Play.svelte';
+  import Pause from 'carbon-icons-svelte/lib/Pause.svelte';
+  import Reset from 'carbon-icons-svelte/lib/Reset.svelte';
+
+  let isSideNavOpen = false;
+
+  const closeSidebar = () => (isSideNavOpen = false);
+</script>
+
+<Header platformName="2:00" bind:isSideNavOpen>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+
+  <HeaderUtilities>
+    <HeaderGlobalAction aria-label="Start" icon={Play} />
+    <HeaderGlobalAction aria-label="Pause" icon={Pause} />
+    <HeaderGlobalAction aria-label="Reset" icon={Reset} />
+  </HeaderUtilities>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+  <SideNavItems>
+    <SideNavLink text="Home" href="/" on:click={closeSidebar} />
+    <SideNavLink text="Routine" href="#/routine" on:click={closeSidebar} />
+    <SideNavLink text="Progress" href="#/progress" on:click={closeSidebar} />
+    <SideNavLink text="Settings" href="#/settings" on:click={closeSidebar} />
+  </SideNavItems>
+</SideNav>
