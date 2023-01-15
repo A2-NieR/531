6d88b63328b0ba8e9564d9cca6c493c0aff77c42
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2023-01-15 13:45:31 +0100

    fix: fix linter warnings/errors

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { calculateWeights, increaseWeights } from './utils';
 
 const weights = {
