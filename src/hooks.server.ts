7064ce88e88b347a63e3e16884b95f48c3b1d13e
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2023-01-18 21:33:03 +0100

    refactor: temporarily disable sentry for build

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,8 @@
 import { redirect, type Handle, type HandleServerError } from '@sveltejs/kit';
 import PocketBase from 'pocketbase';
-import * as Sentry from '@sentry/node';
+// import * as Sentry from '@sentry/node';
 import { PUBLIC_BACKEND_URL } from '$env/static/public';
-import { SENTRY_DSN } from '$env/static/private';
+// import { SENTRY_DSN } from '$env/static/private';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.pb = new PocketBase(PUBLIC_BACKEND_URL);
@@ -28,10 +28,10 @@ export const handle: Handle = async ({ event, resolve }) => {
 	return response;
 };
 
-Sentry.init({ dsn: SENTRY_DSN, tracesSampleRate: 0.5 });
+// Sentry.init({ dsn: SENTRY_DSN, tracesSampleRate: 0.5 });
 
 export const handleError: HandleServerError = ({ error, event }) => {
-	Sentry.captureException(error, { tags: { url: event.url.pathname } });
+	// Sentry.captureException(error, { tags: { url: event.url.pathname } });
 
 	return {
 		code: (error as App.Error).code ?? 'UNKNOWN',
