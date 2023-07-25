02fc74773b70556c94685708a6cb6ea0b47e5947
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2023-07-25 21:15:20 +0200

    feat: add offline fallback

---
+++ src/routes/offline/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Button } from 'carbon-components-svelte';
+	import { WifiOff } from 'carbon-icons-svelte';
+</script>
+
+<div class="heading">
+	<div class="heading-title">
+		<WifiOff size={32} class="heading-icon" />
+		<h2>Currently offline</h2>
+	</div>
+</div>
+<div class="offline-container">
+	<Button type="submit" on:click={() => window.location.replace('/')}>Try again</Button>
+</div>
+
+<style>
+	.offline-container {
+		align-items: center;
+		display: flex;
+		height: 80%;
+		justify-content: center;
+	}
+</style>
